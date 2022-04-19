dfe23d2a4a627fc39d4199cc6c06f6651d197623
ShokiYokota <syoki.yokota@azone-japan.co.jp>
2022-04-19 11:48:22 +0900

    Home型定義

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { getUserId,getUsername} from "../reducks/users/selectors";
+import { RootStateType, UsersStateType } from "../reducks/users/types";
 
 export const Home = () =>{
-  const selector = useSelector(state => state);//<RootStateType, RootStateType>(state => state);
-  const uid = getUserId(selector);
-  const username = getUsername(selector);
+  const selector = useSelector<RootStateType, UsersStateType>(state => state.users);//<RootStateType, RootStateType>(state => state);
+  const uid = getUserId({users: selector});
+  const username = getUsername({users: selector});
 
   return (
     <>
